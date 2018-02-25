31cd3f5cc6d89b7c75750bc886ba6879dc9c7ddc
Bryan <bryanvargasf2110gmail.com>
2018-02-24 19:59:20 -0600

    Cambio a varias divisas y conversion correcta

--- app/src/main/java/com/example/bvarg/convertidor/MainActivity.java
+++ app/src/main/java/com/example/bvarg/convertidor/MainActivity.java
@@ -5,9 +5,11 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,30 +26,28 @@ import devazt.devazt.networking.HttpClient;
 import devazt.devazt.networking.OnHttpRequestComplete;
 import devazt.devazt.networking.Response;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity{
 
-    RadioButton Colon;
-    RadioButton Dolar;
     Button Convertir;
     TextView Total;
+    TextView Total2;
     EditText Cantidad;
     Boolean Moneda = true;
-    float Cambio = 0;
+    Spinner spinner;
+    Spinner spinner2;
+    Resultado r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Colon = (RadioButton)findViewById(R.id.radioButton_Colones);
-        Dolar = (RadioButton)findViewById(R.id.radioButton_Dolares);
         Convertir = (Button) findViewById(R.id.button_Convertir);
         Total = (TextView) findViewById(R.id.textView_Total);
+        Total2 = (TextView) findViewById(R.id.textView_Total2);
         Cantidad = (EditText) findViewById(R.id.editText_Cantidad);
-
-        Colon.setOnClickListener(this);
-        Dolar.setOnClickListener(this);
-        Dolar.setChecked(true);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
 
         HttpClient cliente = new HttpClient(new OnHttpRequestComplete() {
             @Override
@@ -57,8 +57,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     try{
                         JSONObject jsono = new JSONObject(status.getResult());
                         String res = jsono.getString("rates");
-                        Resultado r = gson.fromJson(res, Resultado.class);
-                        Cambio = Float.parseFloat(r.getCRC());
+                        r = gson.fromJson(res, Resultado.class);
+                        //Cambio = Float.parseFloat(r.getCRC());
                         //Total.setText(String.valueOf(Cambio));
                         //JSONArray jsonarray = jsono.getJSONArray("license");
 //                        ArrayList<Resultado> lista = new ArrayList<Resultado>();
@@ -84,18 +84,251 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
         cliente.excecute("https://openexchangerates.org/api/latest.json?app_id=ddb0898d8d1148a495691ba9ad1d4c9f");
 
+        ArrayAdapter spinner_adapter = ArrayAdapter.createFromResource( this, R.array.Moneda, android.R.layout.simple_spinner_item);
+        spinner_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinner_adapter);
+
+        ArrayAdapter spinner_adapter2 = ArrayAdapter.createFromResource( this, R.array.Moneda , android.R.layout.simple_spinner_item);
+        spinner_adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(spinner_adapter2);
     }
 
+    public void convertir(View v) {
+        int numero1 = spinner.getSelectedItemPosition();
+        int numero2 = spinner2.getSelectedItemPosition();
+        double numero;
+        if(!Cantidad.getText().toString().isEmpty()){
+            numero = Double.parseDouble(Cantidad.getText().toString());
+        }
+        else{
+            numero = 0;
+        }
+        Total.setText(String.valueOf(numero) + " =");
+        Cantidad.setText("");
+        double conversor1 = 0;
+        double conversor2 = 0;
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.radioButton_Dolares:
-                Moneda = true;
+        switch (numero1) {
+            case 0:
+                conversor1 = Double.parseDouble(r.get0());
                 break;
-            case R.id.radioButton_Colones:
-                Moneda = false;
+            case 1:
+                conversor1 = Double.parseDouble(r.get1());
+                break;
+            case 2:
+                conversor1 = Double.parseDouble(r.get2());
+                break;
+            case 3:
+                conversor1 = Double.parseDouble(r.get3());
+                break;
+            case 4:
+                conversor1 = Double.parseDouble(r.get4());
+                break;
+            case 5:
+                conversor1 = Double.parseDouble(r.get5());
+                break;
+            case 6:
+                conversor1 = Double.parseDouble(r.get6());
+                break;
+            case 7:
+                conversor1 = Double.parseDouble(r.get7());
+                break;
+            case 8:
+                conversor1 = Double.parseDouble(r.get8());
+                break;
+            case 9:
+                conversor1 = Double.parseDouble(r.get9());
+                break;
+            case 10:
+                conversor1 = Double.parseDouble(r.get10());
+                break;
+            case 11:
+                conversor1 = Double.parseDouble(r.get11());
+                break;
+            case 12:
+                conversor1 = Double.parseDouble(r.get12());
+                break;
+            case 13:
+                conversor1 = Double.parseDouble(r.get13());
+                break;
+            case 14:
+                conversor1 = Double.parseDouble(r.get14());
+                break;
+            case 15:
+                conversor1 = Double.parseDouble(r.get15());
+                break;
+            case 16:
+                conversor1 = Double.parseDouble(r.get16());
+                break;
+            case 17:
+                conversor1 = Double.parseDouble(r.get17());
+                break;
+            case 18:
+                conversor1 = Double.parseDouble(r.get18());
+                break;
+            case 19:
+                conversor1 = Double.parseDouble(r.get19());
+                break;
+            case 20:
+                conversor1 = Double.parseDouble(r.get20());
+                break;
+            case 21:
+                conversor1 = Double.parseDouble(r.get21());
+                break;
+            case 22:
+                conversor1 = Double.parseDouble(r.get22());
+                break;
+            case 23:
+                conversor1 = Double.parseDouble(r.get23());
+                break;
+            case 24:
+                conversor1 = Double.parseDouble(r.get24());
+                break;
+            case 25:
+                conversor1 = Double.parseDouble(r.get25());
+                break;
+            case 26:
+                conversor1 = Double.parseDouble(r.get26());
+                break;
+            case 27:
+                conversor1 = Double.parseDouble(r.get27());
+                break;
+            case 28:
+                conversor1 = Double.parseDouble(r.get28());
+                break;
+            case 29:
+                conversor1 = Double.parseDouble(r.get29());
+                break;
+            case 30:
+                conversor1 = Double.parseDouble(r.get30());
+                break;
+            case 31:
+                conversor1 = Double.parseDouble(r.get31());
+                break;
+            case 32:
+                conversor1 = Double.parseDouble(r.get32());
+                break;
+            case 33:
+                conversor1 = Double.parseDouble(r.get33());
+                break;
+            case 34:
+                conversor1 = Double.parseDouble(r.get34());
+                break;
+            case 35:
+                conversor1 = Double.parseDouble(r.get35());
                 break;
         }
+        switch (numero2) {
+            case 0:
+                conversor2 = Double.parseDouble(r.get0());
+                break;
+            case 1:
+                conversor2 = Double.parseDouble(r.get1());
+                break;
+            case 2:
+                conversor2 = Double.parseDouble(r.get2());
+                break;
+            case 3:
+                conversor2 = Double.parseDouble(r.get3());
+                break;
+            case 4:
+                conversor2 = Double.parseDouble(r.get4());
+                break;
+            case 5:
+                conversor2 = Double.parseDouble(r.get5());
+                break;
+            case 6:
+                conversor2 = Double.parseDouble(r.get6());
+                break;
+            case 7:
+                conversor2 = Double.parseDouble(r.get7());
+                break;
+            case 8:
+                conversor2 = Double.parseDouble(r.get8());
+                break;
+            case 9:
+                conversor2 = Double.parseDouble(r.get9());
+                break;
+            case 10:
+                conversor2 = Double.parseDouble(r.get10());
+                break;
+            case 11:
+                conversor2 = Double.parseDouble(r.get11());
+                break;
+            case 12:
+                conversor2 = Double.parseDouble(r.get12());
+                break;
+            case 13:
+                conversor2 = Double.parseDouble(r.get13());
+                break;
+            case 14:
+                conversor2 = Double.parseDouble(r.get14());
+                break;
+            case 15:
+                conversor2 = Double.parseDouble(r.get15());
+                break;
+            case 16:
+                conversor2 = Double.parseDouble(r.get16());
+                break;
+            case 17:
+                conversor2 = Double.parseDouble(r.get17());
+                break;
+            case 18:
+                conversor2 = Double.parseDouble(r.get18());
+                break;
+            case 19:
+                conversor2 = Double.parseDouble(r.get19());
+                break;
+            case 20:
+                conversor2 = Double.parseDouble(r.get20());
+                break;
+            case 21:
+                conversor2 = Double.parseDouble(r.get21());
+                break;
+            case 22:
+                conversor2 = Double.parseDouble(r.get22());
+                break;
+            case 23:
+                conversor2 = Double.parseDouble(r.get23());
+                break;
+            case 24:
+                conversor2 = Double.parseDouble(r.get24());
+                break;
+            case 25:
+                conversor2 = Double.parseDouble(r.get25());
+                break;
+            case 26:
+                conversor2 = Double.parseDouble(r.get26());
+                break;
+            case 27:
+                conversor2 = Double.parseDouble(r.get27());
+                break;
+            case 28:
+                conversor2 = Double.parseDouble(r.get28());
+                break;
+            case 29:
+                conversor2 = Double.parseDouble(r.get29());
+                break;
+            case 30:
+                conversor2 = Double.parseDouble(r.get30());
+                break;
+            case 31:
+                conversor2 = Double.parseDouble(r.get31());
+                break;
+            case 32:
+                conversor2 = Double.parseDouble(r.get32());
+                break;
+            case 33:
+                conversor2 = Double.parseDouble(r.get33());
+                break;
+            case 34:
+                conversor2 = Double.parseDouble(r.get34());
+                break;
+            case 35:
+                conversor2 = Double.parseDouble(r.get35());
+                break;
+        }
+        numero = (numero/conversor1)*conversor2;
+        Total2.setText(String.valueOf(numero));
     }
 }
--- app/src/main/java/com/example/bvarg/convertidor/Resultado.java
+++ app/src/main/java/com/example/bvarg/convertidor/Resultado.java
@@ -5,9 +5,184 @@ package com.example.bvarg.convertidor;
  */
 
 public class Resultado {
-    private String CRC;
+    private String AED; //"United Arab Emirates Dirham",
+    private String ANG; //"Netherlands Antillean Guilder",
+    private String ARS; //"Argentine Peso",
+    private String AUD; //"Australian Dollar",
+    private String BOB; //"Bolivian Boliviano",
+    private String BRL; //"Brazilian Real",
+    private String BZD; //"Belize Dollar",
+    private String CAD; //"Canadian Dollar",
+    private String CLP; //"Chilean Peso",
+    private String CNY; //"Chinese Yuan",
+    private String COP; //"Colombian Peso",
+    private String CRC; //"Costa Rican Colón",
+    private String CUP; //"Cuban Peso",
+    private String EUR; //"Euro",
+    private String GTQ; //"Guatemalan Quetzal",
+    private String HKD; //"Hong Kong Dollar",
+    private String HNL; //"Honduran Lempira",
+    private String HRK; //"Croatian Kuna",
+    private String JMD; //"Jamaican Dollar",
+    private String JPY; //"Japanese Yen",
+    private String KPW; //"North Korean Won",
+    private String KRW; //"South Korean Won",
+    private String MXN; //"Mexican Peso",
+    private String NIO; //"Nicaraguan Córdoba",
+    private String NZD; //"New Zealand Dollar",
+    private String PAB; //"Panamanian Balboa",
+    private String PEN; //"Peruvian Nuevo Sol",
+    private String PYG; //"Paraguayan Guarani",
+    private String RUB; //"Russian Ruble",
+    private String SGD; //"Singapore Dollar",
+    private String SVC; //"Salvadoran Colón",
+    private String TTD; //"Trinidad and Tobago Dollar",
+    private String UYU; //"Uruguayan Peso",
+    private String VEF; //"Venezuelan Bolívar Fuerte",
+    private String ZAR; //"South African Rand",
+    private String USD; //"United States Dollar",
 
-    public String getCRC(){
+    public String get0() {
+        return AED;
+    }
+
+    public String get1() {
+        return ANG;
+    }
+
+    public String get2() {
+        return ARS;
+    }
+
+    public String get3() {
+        return AUD;
+    }
+
+    public String get4() {
+        return BOB;
+    }
+
+    public String get5() {
+        return BRL;
+    }
+
+    public String get6() {
+        return BZD;
+    }
+
+    public String get7() {
+        return CAD;
+    }
+
+    public String get8() {
+        return CLP;
+    }
+
+    public String get9() {
+        return CNY;
+    }
+
+    public String get10() {
+        return COP;
+    }
+
+    public String get11() {
         return CRC;
+    }
+
+    public String get12() {
+        return CUP;
+    }
+
+    public String get13() {
+        return EUR;
+    }
+
+    public String get14() {
+        return GTQ;
+    }
+
+    public String get15() {
+        return HKD;
+    }
+
+    public String get16() {
+        return HNL;
+    }
+
+    public String get17() {
+        return HRK;
+    }
+
+    public String get18() {
+        return JMD;
+    }
+
+    public String get19() {
+        return JPY;
+    }
+
+    public String get20() {
+        return KPW;
+    }
+
+    public String get21() {
+        return KRW;
+    }
+
+    public String get22() {
+        return MXN;
+    }
+
+    public String get23() {
+        return NIO;
+    }
+
+    public String get24() {
+        return NZD;
+    }
+
+    public String get25() {
+        return PAB;
+    }
+
+    public String get26() {
+        return PEN;
+    }
+
+    public String get27() {
+        return PYG;
+    }
+
+    public String get28() {
+        return RUB;
+    }
+
+    public String get29() {
+        return SGD;
+    }
+
+    public String get30() {
+        return SVC;
+    }
+
+    public String get31() {
+        return TTD;
+    }
+
+    public String get32() {
+        return UYU;
+    }
+
+    public String get33() {
+        return VEF;
+    }
+
+    public String get34() {
+        return ZAR;
+    }
+
+    public String get35() {
+        return USD;
     }
 }
